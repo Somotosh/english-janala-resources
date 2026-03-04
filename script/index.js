14fa582f4375375199d8c7f52d623f171a8b3814
Somotosh <somotosh212@gmail.com>
2026-03-04 21:26:31 +0600

    added Speak your Vocabularies

--- script/index.js
+++ script/index.js
@@ -11,6 +11,11 @@ const manageSpinner =(status) =>{
     document.getElementById('spinner').classList.add("hidden")
   }
 }
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
 const loadLesson = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all')
         .then(res => res.json())
@@ -91,7 +96,7 @@ const displayLavleWorld = (words) => {
             <div class="text-2xl font-medium font-bangla">${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"}/ ${word.pronunciation ? word.pronunciation : "prounciation পাওয়া যায়নি"}</div>
             <div class="flex justify-between items-center">
                 <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+                <button  onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
 
         </div> `
