5b7b619a0a95ea629aefdab993cce0e4f5da375d
Somotosh <somotosh212@gmail.com>
2026-03-04 21:02:53 +0600

    :added search section

--- script/index.js
+++ script/index.js
@@ -122,3 +122,21 @@ const displayLesson = (lessons) => {
 
 }
 loadLesson()
+
+
+document.getElementById("btn-search").addEventListener("click",()=>{
+    removeAtive();
+    const input = document.getElementById("input-search");
+    const searchValue =input.value.trim().toLowerCase();
+    // console.log(searchValue)
+
+    fetch("https://openapi.programming-hero.com/api/words/all")
+    .then(res => res.json())
+    .then(data=> {
+        const allWords = data.data;
+        const filtarWords = allWords.filter(word=>word.word.toLowerCase().includes(searchValue));
+        
+        displayLavleWorld(filtarWords)
+
+    });
+})
