88368e62223c1799a091d1b22864ad58585ee922
Somotosh <somotosh212@gmail.com>
2026-03-04 09:37:36 +0600

    added  lesson no selected section and no vocabulary section

--- script/index.js
+++ script/index.js
@@ -6,6 +6,7 @@ const loadLesson =()=>{
     
 }
 const loadLavelWorld =(id) =>{
+    // console.log(id)
     const url =`https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
     .then(res=> res.json())
@@ -14,15 +15,24 @@ const loadLavelWorld =(id) =>{
 const displayLavleWorld =(words)=>{
     const wordsCoantainer =document.getElementById("words-container");
     wordsCoantainer.innerHTML=""
+
+    if(words.length == 0){
+        wordsCoantainer.innerHTML=` <div class="text-center col-span-full py-10 space-y-6">
+            <img class="mx-auto" src="./assets/alert-error.png" alt="">
+            <p class="font-bangla text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bangla font-bold text-4xl ">নেক্সট Lesson এ যান</h2>
+        </div>`
+        return
+    }
     words.forEach(word => {
         console.log(word)
         const card = document.createElement("div")
         card.innerHTML=`
         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-3">
             <h2 class="font-bold text-2xl">${word.word}</h2>
-            <p class="font-semibold">${word.meaning}</p>
+            <p class="font-semibold">Meaning /Pronounciation</p>
 
-            <div class="text-2xl font-medium font-bangla">${word.pronunciation}</div>
+            <div class="text-2xl font-medium font-bangla">${word.meaning}/ ${word.pronunciation}</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
